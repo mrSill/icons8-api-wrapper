7ce4bf6716414122ca32489a292f89fa3d2e8882
Aliaksandr Sidaruk <aliaksandr.siadaruk@yandex.ru>
2017-11-03 23:19:14 +0300

    Refactoring request tests3

--- tests/Request/AbstractRequestTest.php
+++ tests/Request/AbstractRequestTest.php
@@ -26,73 +26,73 @@ class AbstractRequestTest extends TestCase
             ->will($this->returnValue(Response::class));
     }
 
-    public function testSetAuth()
-    {
-        $stub = $this->getAbstractRequestMock();
+    // public function testSetAuth()
+    // {
+    //     $stub = $this->getAbstractRequestMock();
 
-        $this->assertAttributeSame(
-            [Request::AUTH_HEADER_NAME => self::AUTH_TOKEN],
-            'headers',
-            $stub->setAuth(self::AUTH_TOKEN),
-            'Auth with default type is fail'
-        );
+    //     $this->assertAttributeSame(
+    //         [Request::AUTH_HEADER_NAME => self::AUTH_TOKEN],
+    //         'headers',
+    //         $stub->setAuth(self::AUTH_TOKEN),
+    //         'Auth with default type is fail'
+    //     );
 
-        $this->assertAttributeSame(
-            [Request::AUTH_HEADER_NAME => self::AUTH_TOKEN],
-            'headers',
-            $stub->setAuth(self::AUTH_TOKEN, Request::AUTH_WITH_HEADER),
-            'Auth with http header is fail'
-        );
+    //     $this->assertAttributeSame(
+    //         [Request::AUTH_HEADER_NAME => self::AUTH_TOKEN],
+    //         'headers',
+    //         $stub->setAuth(self::AUTH_TOKEN, Request::AUTH_WITH_HEADER),
+    //         'Auth with http header is fail'
+    //     );
 
-        $this->assertAttributeSame(
-            [Request::AUTH_COOKIE_NAME => self::AUTH_TOKEN],
-            'cookies',
-            $stub->setAuth(self::AUTH_TOKEN, Request::AUTH_WITH_COOKIE),
-            'Auth with cookie is fail'
-        );
+    //     $this->assertAttributeSame(
+    //         [Request::AUTH_COOKIE_NAME => self::AUTH_TOKEN],
+    //         'cookies',
+    //         $stub->setAuth(self::AUTH_TOKEN, Request::AUTH_WITH_COOKIE),
+    //         'Auth with cookie is fail'
+    //     );
 
-        $this->assertAttributeSame(
-            [Request::AUTH_QUERY_NAME => self::AUTH_TOKEN],
-            'queries',
-            $stub->setAuth(self::AUTH_TOKEN, Request::AUTH_WITH_QUERY),
-            'Auth with http query is fail'
-        );
+    //     $this->assertAttributeSame(
+    //         [Request::AUTH_QUERY_NAME => self::AUTH_TOKEN],
+    //         'queries',
+    //         $stub->setAuth(self::AUTH_TOKEN, Request::AUTH_WITH_QUERY),
+    //         'Auth with http query is fail'
+    //     );
 
-        try {
-            // Empty auth token
-            $stub->setAuth(null);
-            // Unsupported auth method
-            $stub->setAuth(self::AUTH_TOKEN, 'Unsupported');
-            // Impossible. Set FAIL
-            $this->assertTrue(false);
-        } catch (\Exception $e) {
-            $this->throwException($e);
-        }
-    }
+    //     try {
+    //         // Empty auth token
+    //         $stub->setAuth(null);
+    //         // Unsupported auth method
+    //         $stub->setAuth(self::AUTH_TOKEN, 'Unsupported');
+    //         // Impossible. Set FAIL
+    //         $this->assertTrue(false);
+    //     } catch (\Exception $e) {
+    //         $this->throwException($e);
+    //     }
+    // }
 
-    public function testSetQuery()
-    {
-        $stub = $this->getAbstractRequestMock();
+    // public function testSetQuery()
+    // {
+    //     $stub = $this->getAbstractRequestMock();
 
-        $query = [
-            'foo' => 'bar',
-            'bar' => 'foo'
-        ];
+    //     $query = [
+    //         'foo' => 'bar',
+    //         'bar' => 'foo'
+    //     ];
 
-        // Set queries as array
-        $this->assertAttributeSame(
-            $query,
-            'queries',
-            $stub->setQuery($query)
-        );
+    //     // Set queries as array
+    //     $this->assertAttributeSame(
+    //         $query,
+    //         'queries',
+    //         $stub->setQuery($query)
+    //     );
 
-        $query['bar'] = 'bar';
+    //     $query['bar'] = 'bar';
 
-        // Set query as key-value
-        $this->assertAttributeSame(
-            $query,
-            'queries',
-            $stub->setQuery('bar', 'bar')
-        );
-    }
+    //     // Set query as key-value
+    //     $this->assertAttributeSame(
+    //         $query,
+    //         'queries',
+    //         $stub->setQuery('bar', 'bar')
+    //     );
+    // }
 }
--- tests/Request/RequestTest.php
+++ tests/Request/RequestTest.php
@@ -11,21 +11,94 @@ class RequestTest extends TestCase
     public function testRequest()
     {
         $request = new Request();
-        $this->assertInstanceOf('mrSill\Icons8\Request\Request', $request);
 
-        $request->setQuery(['platform' => Platform::IOS7_PLATFORM]);
+        // Deprecated auth method
+        $this->assertAttributeSame(
+            self::AUTH_TOKEN,
+            'authToken',
+            $request->setAuthToken(self::AUTH_TOKEN)
+        );
 
-        $request->setQuery('platform', Platform::ALL_PLATFORMS);
-
-        $response = $request->request('categories');
-        $this->assertInstanceOf('mrSill\Icons8\Response\Response', $response);
+        $this->assertInstanceOf(
+            'mrSill\Icons8\Response\Response',
+            $request->request('categories')
+        );
     }
 
-    public function testDeprecatedSetAuth()
+    public function testSetQuery()
     {
         $request = new Request();
 
-        // Deprecated auth method
-        $request->setAuthToken(self::AUTH_TOKEN);
+        $query = ['platform' => Platform::IOS7_PLATFORM];
+        $this->assertAttributeSame(
+            $query,
+            'queries',
+            $request->setQuery(['platform' => Platform::IOS7_PLATFORM]),
+            'Set query fail'
+        );
+
+        $query['platform'] = Platform::ALL_PLATFORMS;
+        $this->assertAttributeSame(
+            $query,
+            'queries',
+            $request->setQuery('platform', Platform::ALL_PLATFORMS),
+            'Reset query value fail'
+        );
+    }
+
+    public function testFailSetAuth()
+    {
+        $request = new Request();
+
+        try {
+            // Empty auth token
+            $request->setAuth(null);
+
+            // Impossible. Set FAIL
+            $this->assertTrue(false);
+        } catch (\Exception $e) {
+            $this->throwException($e);
+        }
+
+        try {
+            // Unsupported auth method
+            $request->setAuth(self::AUTH_TOKEN, 'Unsupported');
+
+            // Impossible. Set FAIL
+            $this->assertTrue(false);
+        } catch (\Exception $e) {
+            $this->throwException($e);
+        }
+    }
+
+    public function testRequestWithAuthQuery()
+    {
+        $request = new Request();
+
+        $this->assertAttributeSame(
+            [Request::AUTH_HEADER_NAME => self::AUTH_TOKEN],
+            'headers',
+            $request->setAuth(self::AUTH_TOKEN),
+            'Auth with http header fail'
+        );
+
+        $this->assertAttributeSame(
+            [Request::AUTH_COOKIE_NAME => self::AUTH_TOKEN],
+            'cookies',
+            $request->setAuth(self::AUTH_TOKEN, Request::AUTH_WITH_COOKIE),
+            'Auth with cookie is fail'
+        );
+
+        $this->assertAttributeSame(
+            [Request::AUTH_QUERY_NAME => self::AUTH_TOKEN],
+            'queries',
+            $request->setAuth(self::AUTH_TOKEN, Request::AUTH_WITH_QUERY),
+            'Auth with http query is fail'
+        );
+
+        $this->assertInstanceOf(
+            'mrSill\Icons8\Response\Response',
+            $request->request('categories')
+        );
     }
 }
