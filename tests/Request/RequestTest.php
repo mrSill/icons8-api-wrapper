8e896b2a097d763cb80ab588f16ab1948e155670
Aliaksandr Sidaruk <aliaksandr.siadaruk@yandex.ru>
2017-11-03 10:16:26 +0300

    Configure phpUnit. Add request tests

---
+++ tests/Request/RequestTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests\Request;
+
+use mrSill\Icons8\Request\Request;
+
+class RequestTest extends \PHPUnit_Framework_TestCase
+{
+    public function testRequest()
+    {
+        $request = new Request();
+        $this->assertInstanceOf('mrSill\Icons8\Request\Request', $request);
+
+        $response = $request->request('categories');
+        $this->assertInstanceOf('mrSill\Icons8\Response\Response', $response);
+    }
+
+    public function testAuth()
+    {
+        $request = new Request();
+
+        try {
+            $request->setAuth(ICONS8_TEST_TOKEN);
+            $this->assertTrue(true);
+        } catch (\Exception $e) {
+            echo $e->getMessage();
+            $this->assertTrue(false);
+        }
+    }
+
+    public function testWrongAuth()
+    {
+        $request = new Request();
+
+        try {
+            // Wrong Auth type
+            $request->setAuth(ICONS8_TEST_TOKEN, 'Unsupported_Auth_Type');
+            $this->assertTrue(false);
+
+            // Wrong Auth token
+            $request->setAuth(null, Request::AUTH_WITH_HEADER);
+            $this->assertTrue(false);
+        } catch (\Exception $e) {
+            $this->assertTrue(true);
+        }
+    }
+}
