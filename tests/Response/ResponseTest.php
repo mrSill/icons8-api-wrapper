9a351eab37a6a0ed2d6ac3b6afd14cb114282e86
Aliaksandr Sidaruk <aliaksandr.siadaruk@yandex.ru>
2017-11-07 20:02:02 +0300

    Add response tests

--- tests/Response/ResponseTest.php
+++ tests/Response/ResponseTest.php
@@ -3,26 +3,36 @@
 namespace Tests\Response;
 
 use mrSill\Icons8\Request\Request;
+use mrSill\Icons8\Response\JsonBodyResponse;
 use mrSill\Icons8\Response\Response;
 use mrSill\Icons8\Response\XmlBodyResponse;
+use Tests\TestCase;
 
 /**
  * Class ResponseTest
  *
  * @package Tests\Response
  */
-class ResponseTest extends \PHPUnit_Framework_TestCase
+class ResponseTest extends TestCase
 {
-    protected function getResponse()
+    protected $endpoints = [
+        'v1' => 'https://api.icons8.com/api/iconsets/categories',
+        'v2' => 'https://api.icons8.com/api/iconsets/v2/categories',
+        'v3' => 'https://api.icons8.com/api/iconsets/v3/categories',
+    ];
+
+    protected function getResponse($uri)
     {
         $request = new Request();
 
-        return $request->request('categories');
+        $response = $request->request($uri);
+
+        return $response;
     }
 
-    public function testV1Response()
+    public function testResponse()
     {
-        $response  = $this->getResponse();
+        $response = $this->getResponse($this->endpoints['v1']);
 
         $this->assertInstanceOf(
             Response::class,
@@ -33,22 +43,74 @@ class ResponseTest extends \PHPUnit_Framework_TestCase
             \GuzzleHttp\Psr7\Response::class,
             $response->getResponse()
         );
+    }
+
+    public function testStatusCode()
+    {
+        $response = $this->getResponse($this->endpoints['v1']);
+
         // check status code
         $this->assertTrue($response->hasStatusCode(200));
         $this->assertEquals($response->getStatusCode(), 200);
+    }
 
-        // check content type
-        $this->assertEquals($response->getContentType(), Response::XML_CONTENT_TYPE);
+    public function testErrors()
+    {
+        $response  = $this->getResponse($this->endpoints['v1']);
 
         $this->assertFalse($response->hasErrors());
         $this->assertNull($response->getError());
+    }
 
-        // $this->setExpectedException(\Exception::class);
+    public function testGetBody()
+    {
+        $response  = $this->getResponse($this->endpoints['v1']);
 
         $this->assertInstanceOf(
             XmlBodyResponse::class,
             $response->getBody()
         );
+
+        $response  = $this->getResponse($this->endpoints['v2']);
+
+        $this->assertInstanceOf(
+            JsonBodyResponse::class,
+            $response->getBody()
+        );
+
+        $response  = $this->getResponse($this->endpoints['v3']);
+
+        $this->assertInstanceOf(
+            JsonBodyResponse::class,
+            $response->getBody()
+        );
+    }
+
+    public function testXMLBody()
+    {
+        $response  = $this->getResponse($this->endpoints['v1']);
+
+        // check content type
+        $this->assertEquals($response->getContentType(), Response::XML_CONTENT_TYPE);
+
+        // $this->setExpectedException(\Exception::class);
+
+        $this->assertTrue(is_array($response->getBody()->toArray()));
+    }
+
+    public function testJSONBody()
+    {
+        $response  = $this->getResponse($this->endpoints['v2']);
+
+        // check content type
+        $this->assertEquals($response->getContentType(), Response::JSON_CONTENT_TYPE);
+
+        $response  = $this->getResponse($this->endpoints['v3']);
+
+        // check content type
+        $this->assertEquals($response->getContentType(), Response::JSON_CONTENT_TYPE);
+
+        // $this->setExpectedException(\Exception::class);
 
         $this->assertTrue(is_array($response->getBody()->toArray()));
     }
